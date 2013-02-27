50ac884c8c6bea94c01b3b86d36c092c11412bb3
Alexis Beingessner <a.beingessner@gmail.com>
2013-02-27 18:55:54 -0500

    Fleshing out model APIs

--- src/main/java/com/steamedpears/comp3004/Card.java
+++ src/main/java/com/steamedpears/comp3004/Card.java
@@ -1,8 +1,19 @@
 package com.steamedpears.comp3004;
 
+import com.google.gson.JsonObject;
+
+import java.awt.Image;
+import java.util.Map;
+
 public class Card {
     private String color;
     private String name;
+    private Image image;
+    private Map<String, Integer> baseAssets;
+
+    public Card(JsonObject obj){
+        //TODO: interpret this object
+    }
 
     public String getName(){
         return name;
@@ -10,5 +21,15 @@ public class Card {
 
     public String getColor(){
         return color;
+    }
+
+    public Image getImage(){
+        return image;
+    }
+
+    public Map<String, Integer> getAssets(Player player){
+        //TODO: compute the assets this card yields if played by this player
+
+        return null;
     }
 }
--- src/main/java/com/steamedpears/comp3004/Player.java
+++ src/main/java/com/steamedpears/comp3004/Player.java
@@ -1,14 +1,85 @@
 package com.steamedpears.comp3004;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
-public class Player {
+import static com.steamedpears.comp3004.Asset.*;
+
+public abstract class Player {
+    //inner utility classes/////////////////////////////////////////////////
+    protected enum PlayerCardAction{
+        DISCARD, BUILD, PLAY
+    }
+
+    protected class PlayerCommand{
+        public PlayerCardAction action;
+        public Card card;
+        public Map<String, Integer> leftPurchases;
+        public Map<String, Integer> rightPurchases;
+    }
+
+
+    //instance variables////////////////////////////////////////////////////
     private Wonder wonder;
-    private ArrayList<Card> playedCards;
-    private ArrayList<Card> cardsInHand;
+    private List<Card> playedCards;
     private Map<String, Integer> freeAssets;
     private Map<String, Integer> restrictedAssets;
+    private Player playerLeft;
+    private Player playerRight;
+
+    //constructor///////////////////////////////////////////////////////////
+    public Player(Wonder wonder){
+        this.wonder = wonder;
+        this.playedCards = new ArrayList<Card>();
+    }
+
+    private final void discardCard(Card card){
+        //TODO: remove card from hand, get 3 gold
+    }
+
+    private final void buildWonder(Card card){
+        //TODO: buildup wonder, return whether successful
+    }
+
+    private final void playCard(Card card){
+        //TODO: play card, return whether successful
+    }
+
+    public final void takeTurn(ArrayList<Card> cards){
+        PlayerCommand command = handleTurn(cards);
+        if(command!=null){
+            if(command.action.equals(PlayerCardAction.BUILD)){
+                buildWonder(command.card);
+            }else if(command.action.equals(PlayerCardAction.PLAY)){
+                playCard(command.card);
+            }else if(command.action.equals(PlayerCardAction.DISCARD)){
+                discardCard(command.card);
+            }
+        }
+    }
+
+    //extend with a GUI or AI
+    protected abstract PlayerCommand handleTurn(ArrayList<Card> cardsInHand);
+
+    //setters///////////////////////////////////////////////////////////////////
+    public final void setPlayerLeft(Player playerLeft){
+        this.playerLeft = playerLeft;
+    }
+
+    public final void setPlayerRight(Player playerRight){
+        this.playerRight = playerRight;
+    }
+
+    //getters///////////////////////////////////////////////////////////////////
+    public final Player getPlayerLeft(){
+        return playerLeft;
+    }
+
+    public final Player getPlayerRight(){
+        return playerRight;
+    }
+
 
 
 }
